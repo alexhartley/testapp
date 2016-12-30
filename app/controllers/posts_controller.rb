50762710efb9af34f9205fd563601898b7060037
alexhartley <alexhartley@gmail.com>
2016-12-30 14:37:01 +0000

    Add validation to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,11 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create!(post_params)
+    @post = Post.create(post_params)
     if @post.save
-      redirect_to 'index'
+      redirect_to posts_path
     else
-      render :edit
+      render action: "new"
     end
   end
 
